1722025924d6456204684228af62d78795efe485
Oskar Gewalli <oskar@gewalli.se>
2015-03-09 07:38:36 +0100

    Why did this file not get commited?

---
+++ Core/Commands/RepositoryCommandHandler.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace SomeBasicFileStoreApp.Core.Commands
+{
+    public class RepositoryCommandHandler:ICommandHandler<Command>
+    {
+        private readonly IRepository repository;
+        public RepositoryCommandHandler(IRepository repository)
+        {
+            this.repository = repository;
+        }
+
+        public void Handle(Command command)
+        {
+            command.Handle(repository);
+        }
+    }
+}
+
