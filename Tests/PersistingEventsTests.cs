38a32a6213ab1fecf1501ec544675f0dcd77b64d
Oskar Gewalli <oskar@gewalli.se>
2015-03-10 07:16:57 +0100

    Using with in test library as well

--- Tests/PersistingEventsTests.cs
+++ Tests/PersistingEventsTests.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using SomeBasicFileStoreApp.Core;
 using System.Linq;
 using SomeBasicFileStoreApp.Core.Commands;
-
+using With;
 namespace SomeBasicFileStoreApp.Tests
 {
 	[TestFixture]
@@ -18,30 +18,12 @@ namespace SomeBasicFileStoreApp.Tests
 			File.WriteAllText("CustomerDataTests.db", string.Empty); 
 		}
 
-		private IEnumerable<IEnumerable<T>> BatchesOf<T>(IEnumerable<T> xs, int count)
-		{
-			var enumerator = xs.GetEnumerator();
-            while (true)
-			{
-				var list = new List<T>(count);
-				for (int i = 0; i < count && enumerator.MoveNext(); i++)
-				{
-					list.Add(enumerator.Current);
-                }
-				if (!list.Any())
-				{
-					break;
-				}
-				yield return list;
-			}
-		}
-
 		[Test]
 		public void Read_items_persisted_in_separate_batches()
 		{
 			var commands = new GetCommands().Get().ToArray();
 			_persist = new AppendToFile("CustomerDataTests.db");
-			var batches = BatchesOf(commands, 3).ToArray();
+            var batches = commands.BatchesOf(3).ToArray();
 			// in order for the test to be valid
 			Assert.That(batches.Length, Is.GreaterThan(2));
 			foreach (var batch in batches)
