91471383073897a0d306d0749c1ddc5a6859e4b5
Oskar Gewalli <oskar@gewalli.se>
2018-10-12 08:24:42 +0200

    Cleaning

--- Core/Domain/Repository.cs
+++ Core/Domain/Repository.cs
@@ -5,39 +5,21 @@ namespace SomeBasicFileStoreApp.Core
 {
     public class Repository : IRepository
     {
-        private IDictionary<long, Customer> _customers = new ConcurrentDictionary<long, Customer>();
-        private IDictionary<long, Product> _products = new ConcurrentDictionary<long, Product>();
-        private IDictionary<long, Order> _orders = new ConcurrentDictionary<long, Order>();
+        private readonly IDictionary<long, Customer> _customers = new ConcurrentDictionary<long, Customer>();
+        private readonly IDictionary<long, Product> _products = new ConcurrentDictionary<long, Product>();
+        private readonly IDictionary<long, Order> _orders = new ConcurrentDictionary<long, Order>();
 
-        public void Save(Customer obj)
-        {
-            _customers[obj.Id] = obj;
-        }
+        public void Save(Customer obj) => _customers[obj.Id] = obj;
 
-        public IEnumerable<Customer> GetCustomers()
-        {
-            return _customers.Values;
-        }
+        public IEnumerable<Customer> GetCustomers() => _customers.Values;
 
-        public IEnumerable<Order> GetOrders()
-        {
-            return _orders.Values;
-        }
+        public IEnumerable<Order> GetOrders() => _orders.Values;
 
-        public IEnumerable<Product> GetProducts()
-        {
-            return _products.Values;
-        }
+        public IEnumerable<Product> GetProducts() => _products.Values;
 
-        public void Save(Product obj)
-        {
-            _products[obj.Id] = obj;
-        }
+        public void Save(Product obj) => _products[obj.Id] = obj;
 
-        public void Save(Order obj)
-        {
-            _orders[obj.Id] = obj;
-        }
+        public void Save(Order obj) => _orders[obj.Id] = obj;
 
         public bool TryGetCustomer(int customerId, out Customer customer) =>
             _customers.TryGetValue(customerId, out customer);
